50e94ef0352629bfb1558c9718c5949626d77e83
manivasagam <s.kishore123.64@gmail.com>
2022-11-02 16:56:45 +0530

    2-11-2022

---
+++ index.js
@@ -0,0 +1,38 @@
+// const os = require("os");
+// console.log(os.cpus());
+const fs = require("fs");
+
+
+// fs.mkdir("./myfolder",function(){
+//     console.log("folder created");
+// })
+
+// fs.rmdir("./myfolder",function(){
+//     console.log("deleted")
+// })
+
+// fs.writeFile('./hi.txt',"hello content",function(err){
+//     if(err) throw err;
+//     console.log("created");
+// })
+
+// fs.readFile("./hi.txt","utf-8",function(err,data){
+//     if(err) throw err;
+//     console.log(data);
+// })
+
+
+// fs.mkdir("./myfolder1",function(){
+//         console.log("folder1 created");
+//     })
+
+//     fs.mkdir("./myfolder2",function(){
+//             console.log("folder2 created");
+//         })
+
+
+// fs.readdir("./",function(err,data){
+//     if(err) throw err;
+//     console.log(data);
+
+// })
